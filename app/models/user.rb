714d402b15359b3f7a74b528551d3449524cad40
chibiProgrammer <nakamura_m@uuum.jp>
2020-11-12 12:01:31 +0900

    anotateの追加と反映

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id            :bigint           not null, primary key
+#  youtuber_name :string
+#  image_url     :string
+#  group_id      :integer
+#  brand_id      :integer
+#  age           :integer
+#  infomation    :string
+#  birthplace    :string
+#  height        :string
+#  youtube_url   :string
+#  deleted_flag  :boolean
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class User < ApplicationRecord
 end
